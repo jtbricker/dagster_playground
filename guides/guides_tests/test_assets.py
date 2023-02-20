1c4eda40a6c54a93caf2d71827fa1ebf6e47490b
Justin Bricker <jt.bricker@gmail.com>
2023-02-20 22:33:04 +0000

    Add tests and requirements file

--- guides/guides_tests/test_assets.py
+++ guides/guides_tests/test_assets.py
@@ -1,1 +1,15 @@
+from guides.assets import (
+    get_total_size,
+    diamond
+)
 
+def test_get_total_size():
+    file_sizes = { 'file1': 400, 'file2': 50 }
+    result = get_total_size(file_sizes)
+    assert result == 450
+
+
+def test_diamond():
+    res = diamond.execute_in_process()
+    assert res.success
+    assert res.output_for_node("get_total_size") > 0
