e1bc15fea5b522819d82e8884a2789000d49deff
Justin Bricker <jt.bricker@gmail.com>
2023-02-20 22:28:15 +0000

    Diamond dep pattern

--- guides/guides/__init__.py
+++ guides/guides/__init__.py
@@ -1,21 +1,8 @@
-import os
+from dagster import Definitions, load_assets_from_modules
 
-from dagster import get_dagster_logger, job, op
+from . import assets
 
-
-@op
-def get_file_sizes():
-    files = [f for f in os.listdir(".") if os.path.isfile(f)]
-    return {f: os.path.getsize(f) for f in files}
-
-
-@op
-def report_total_size(file_sizes):
-    total_size = sum(file_sizes.values())
-    # In real life, we'd send an email or Slack message instead of just logging:
-    get_dagster_logger().info(f"Total size: {total_size}")
-
-
-@job
-def serial():
-    report_total_size(get_file_sizes())
+defs = Definitions(
+    assets=load_assets_from_modules([assets]),
+    jobs = [assets.diamond]
+)
--- guides/guides/assets.py
+++ guides/guides/assets.py
@@ -1,17 +1,34 @@
-
 import os
-from dagster import (
-    job,
-    op,
-    get_dagster_logger
-)
+
+from dagster import get_dagster_logger, job, op
+
 
 @op
 def get_file_sizes():
     files = [f for f in os.listdir(".") if os.path.isfile(f)]
-    for f in files:
-        get_dagster_logger().info(f"Size of {f} is {os.path.getsize(f)}")
+    return {f: os.path.getsize(f) for f in files}
+
+
+@op
+def get_total_size(file_sizes):
+    return sum(file_sizes.values())
+
+
+@op
+def get_largest_size(file_sizes):
+    return max(file_sizes.values())
+
+
+@op
+def report_file_stats(total_size, largest_size):
+    # In real life, we'd send an email or Slack message instead of just logging:
+    get_dagster_logger().info(f"Total size: {total_size}, largest size: {largest_size}")
+
 
 @job
-def file_sizes_job():
-    get_file_sizes()
+def diamond():
+    file_sizes = get_file_sizes()
+    report_file_stats(
+        total_size=get_total_size(file_sizes),
+        largest_size=get_largest_size(file_sizes),
+    )
