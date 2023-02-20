ba0e8142d4fed68c33261ea6cbbe0ba240097fa7
Justin Bricker <jt.bricker@gmail.com>
2023-02-20 22:21:15 +0000

    Connected outputs to inputs

--- guides/guides/__init__.py
+++ guides/guides/__init__.py
@@ -1,5 +1,21 @@
-from dagster import Definitions, load_assets_from_modules
+import os
 
-from . import assets
+from dagster import get_dagster_logger, job, op
 
-defs = Definitions(assets=load_assets_from_modules([assets]))
+
+@op
+def get_file_sizes():
+    files = [f for f in os.listdir(".") if os.path.isfile(f)]
+    return {f: os.path.getsize(f) for f in files}
+
+
+@op
+def report_total_size(file_sizes):
+    total_size = sum(file_sizes.values())
+    # In real life, we'd send an email or Slack message instead of just logging:
+    get_dagster_logger().info(f"Total size: {total_size}")
+
+
+@job
+def serial():
+    report_total_size(get_file_sizes())
