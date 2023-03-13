b6d79fc558c06f7474095729456f771700d9a9a4
Justin Bricker <jt.bricker@gmail.com>
2023-03-13 21:55:37 +0000

    Add a downstream asset

--- tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/assets/__init__.py
+++ tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/assets/__init__.py
@@ -1,10 +1,8 @@
-# /tutorial_template/tutorial_dbt_dagster/assets/__init__.py
-
 import pandas as pd
+import plotly.express as px
 from dagster_dbt import load_assets_from_dbt_project
 
-from dagster import asset, file_relative_path
-
+from dagster import AssetIn, MetadataValue, asset, file_relative_path
 
 DBT_PROJECT_PATH = file_relative_path(__file__, "../../jaffle_shop")
 DBT_PROFILES = file_relative_path(__file__, "../../jaffle_shop/config")
@@ -24,3 +22,15 @@ def orders_raw() -> pd.DataFrame:
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["jaffle_shop"]
 )
+
+@asset(
+    ins={"customers": AssetIn(key_prefix=["jaffle_shop"])},
+    group_name="staging",
+)
+def order_count_chart(context, customers: pd.DataFrame) -> None:
+    fig = px.histogram(customers, x="number_of_orders")
+    fig.update_layout(bargap=0.2)
+    save_chart_path = file_relative_path(__file__, "order_count_chart.html")
+    fig.write_html(save_chart_path, auto_open=True)
+
+    context.add_output_metadata({"plot_url": MetadataValue.url("file://" + save_chart_path)})
