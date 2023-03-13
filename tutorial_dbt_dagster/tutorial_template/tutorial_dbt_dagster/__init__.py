f45d357d8d0c301b0f8ede2eab5553b573fd287f
Justin Bricker <jt.bricker@gmail.com>
2023-03-13 19:21:06 +0000

    Load dbt models as Dagster assets

--- tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/__init__.py
+++ tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/__init__.py
@@ -1,2 +1,20 @@
-## Refer to Using dbt with Dagster, part two for info about this file:
-## https://docs.dagster.io/integrations/dbt/using-dbt-with-dagster/part-two
+# /tutorial_template/tutorial_dbt_dagster/__init__.py
+
+import os
+
+from dagster_dbt import dbt_cli_resource
+from tutorial_dbt_dagster import assets
+from tutorial_dbt_dagster.assets import DBT_PROFILES, DBT_PROJECT_PATH
+
+from dagster import Definitions, load_assets_from_modules
+
+resources = {
+    "dbt": dbt_cli_resource.configured(
+        {
+            "project_dir": DBT_PROJECT_PATH,
+            "profiles_dir": DBT_PROFILES,
+        },
+    ),
+}
+
+defs = Definitions(assets=load_assets_from_modules([assets]), resources=resources)
--- tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/assets/__init__.py
+++ tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/assets/__init__.py
@@ -1,2 +1,13 @@
-## Refer to Using dbt with Dagster, part two for info about this file:
-## https://docs.dagster.io/integrations/dbt/using-dbt-with-dagster/part-two
+# /tutorial_template/tutorial_dbt_dagster/assets/__init__.py
+
+from dagster_dbt import load_assets_from_dbt_project
+
+from dagster import file_relative_path
+
+
+DBT_PROJECT_PATH = file_relative_path(__file__, "../../jaffle_shop")
+DBT_PROFILES = file_relative_path(__file__, "../../jaffle_shop/config")
+
+dbt_assets = load_assets_from_dbt_project(
+    project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["jaffle_shop"]
+)
